57609e69d288f2e4416be96ec5d46795788492ec
Lev <lev@levlaz.org>
2026-04-18 00:02:18 -0700

    copy example

--- extensions/extension-demo/src/ExamplePanel.tsx
+++ extensions/extension-demo/src/ExamplePanel.tsx
@@ -1,85 +1,127 @@
-import { Immutable, MessageEvent, PanelExtensionContext, Topic } from "@foxglove/extension";
-import { ReactElement, useEffect, useLayoutEffect, useState } from "react";
+import { PanelExtensionContext, Topic, MessageEvent } from "@foxglove/extension";
+import { CompressedImage } from "@foxglove/schemas";
+import { useLayoutEffect, useEffect, useState, useRef, useMemo } from "react";
 import { createRoot } from "react-dom/client";
 
-function ExamplePanel({ context }: { context: PanelExtensionContext }): ReactElement {
-  const [topics, setTopics] = useState<undefined | Immutable<Topic[]>>();
-  const [messages, setMessages] = useState<undefined | Immutable<MessageEvent[]>>();
+type ImageMessage = MessageEvent<CompressedImage>;
+
+type PanelState = {
+  topic?: string;
+};
+
+// Draws the compressed image data into our canvas.
+async function drawImageOnCanvas(imgData: Uint8Array, canvas: HTMLCanvasElement, format: string) {
+  const ctx = canvas.getContext("2d");
+  if (ctx == undefined) {
+    return;
+  }
+
+  // Create a bitmap from our raw compressed image data.
+  const blob = new Blob([imgData], { type: `image/${format}` });
+  const bitmap = await createImageBitmap(blob);
+
+  // Adjust for aspect ratio.
+  canvas.width = Math.round((canvas.height * bitmap.width) / bitmap.height);
+
+  // Draw the image.
+  ctx.drawImage(bitmap, 0, 0, canvas.width, canvas.height);
+  ctx.resetTransform();
+}
+
+function ExamplePanel({ context }: { context: PanelExtensionContext }): JSX.Element {
+  const [topics, setTopics] = useState<readonly Topic[] | undefined>();
+  const [message, setMessage] = useState<ImageMessage>();
 
   const [renderDone, setRenderDone] = useState<(() => void) | undefined>();
 
-  // We use a layout effect to setup render handling for our panel. We also setup some topic subscriptions.
-  useLayoutEffect(() => {
-    // The render handler is run by the broader Foxglove system during playback when your panel
-    // needs to render because the fields it is watching have changed. How you handle rendering depends on your framework.
-    // You can only setup one render handler - usually early on in setting up your panel.
-    //
-    // Without a render handler your panel will never receive updates.
-    //
-    // The render handler could be invoked as often as 60hz during playback if fields are changing often.
-    context.onRender = (renderState, done) => {
-      // render functions receive a _done_ callback. You MUST call this callback to indicate your panel has finished rendering.
-      // Your panel will not receive another render callback until _done_ is called from a prior render. If your panel is not done
-      // rendering before the next render call, Foxglove shows a notification to the user that your panel is delayed.
-      //
-      // Set the done callback into a state variable to trigger a re-render.
-      setRenderDone(() => done);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
-      // We may have new topics - since we are also watching for messages in the current frame, topics may not have changed
-      // It is up to you to determine the correct action when state has not changed.
+  // Restore our state from the layout via the context.initialState property.
+  const [state, setState] = useState<PanelState>(() => {
+    return context.initialState as PanelState;
+  });
+
+  // Filter all of our topics to find the ones with a CompresssedImage message.
+  const imageTopics = useMemo(
+    () => (topics ?? []).filter((topic) => topic.schemaName === "sensor_msgs/CompressedImage"),
+    [topics],
+  );
+
+  useEffect(() => {
+    // Save our state to the layout when the topic changes.
+    context.saveState({ topic: state.topic });
+
+    if (state.topic) {
+      // Subscribe to the new image topic when a new topic is chosen.
+      context.subscribe([{ topic: state.topic }]);
+    }
+  }, [context, state.topic]);
+
+  // Choose our first available image topic as a default once we have a list of topics available.
+  useEffect(() => {
+    if (state.topic == undefined) {
+      setState({ topic: imageTopics[0]?.name });
+    }
+  }, [state.topic, imageTopics]);
+
+  // Every time we get a new image message draw it to the canvas.
+  useEffect(() => {
+    if (message) {
+      drawImageOnCanvas(message.message.data, canvasRef.current!, message.message.format).catch(
+        (error) => {
+          console.log(error);
+        },
+      );
+    }
+  }, [message]);
+
+  // Setup our onRender function and start watching topics and currentFrame for messages.
+  useLayoutEffect(() => {
+    context.onRender = (renderState, done) => {
+      setRenderDone(() => done);
       setTopics(renderState.topics);
 
-      // currentFrame has messages on subscribed topics since the last render call
-      setMessages(renderState.currentFrame);
+      // Save the most recent message on our image topic.
+      if (renderState.currentFrame && renderState.currentFrame.length > 0) {
+        setMessage(renderState.currentFrame[renderState.currentFrame.length - 1] as ImageMessage);
+      }
     };
 
-    // After adding a render handler, you must indicate which fields from RenderState will trigger updates.
-    // If you do not watch any fields then your panel will never render since the panel context will assume you do not want any updates.
-
-    // tell the panel context that we care about any update to the _topic_ field of RenderState
     context.watch("topics");
-
-    // tell the panel context we want messages for the current frame for topics we've subscribed to
-    // This corresponds to the _currentFrame_ field of render state.
     context.watch("currentFrame");
-
-    // subscribe to some topics, you could do this within other effects, based on input fields, etc
-    // Once you subscribe to topics, currentFrame will contain message events from those topics (assuming there are messages).
-    context.subscribe([{ topic: "/some/topic" }]);
   }, [context]);
 
-  // invoke the done callback once the render is complete
+  // Call our done function at the end of each render.
   useEffect(() => {
     renderDone?.();
   }, [renderDone]);
 
   return (
-    <div style={{ padding: "1rem" }}>
-      <h2>Welcome to your new extension panel!</h2>
-      <p>
-        Check the{" "}
-        <a href="https://docs.foxglove.dev/docs/visualization/extensions/introduction">
-          documentation
-        </a>{" "}
-        for more details on building extension panels for Foxglove.
-      </p>
-      <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr", rowGap: "0.2rem" }}>
-        <b style={{ borderBottom: "1px solid" }}>Topic</b>
-        <b style={{ borderBottom: "1px solid" }}>Schema name</b>
-        {(topics ?? []).map((topic) => (
-          <>
-            <div key={topic.name}>{topic.name}</div>
-            <div key={topic.schemaName}>{topic.schemaName}</div>
-          </>
-        ))}
+    <div style={{ height: "100%", padding: "1rem" }}>
+      <div style={{ paddingBottom: "1rem", display: "flex", gap: "0.5rem", alignItems: "center" }}>
+        <label>Choose a topic to render:</label>
+        <select
+          value={state.topic}
+          onChange={(event) => {
+            setState({ topic: event.target.value });
+          }}
+          style={{ flex: 1 }}
+        >
+          {imageTopics.map((topic) => (
+            <option key={topic.name} value={topic.name}>
+              {topic.name}
+            </option>
+          ))}
+        </select>
       </div>
-      <div>{messages?.length}</div>
+      <canvas width={480} height={480} ref={canvasRef} />
     </div>
   );
 }
 
 export function initExamplePanel(context: PanelExtensionContext): () => void {
   const root = createRoot(context.panelElement);
+
   root.render(<ExamplePanel context={context} />);
 
   // Return a function to run when the panel is removed
